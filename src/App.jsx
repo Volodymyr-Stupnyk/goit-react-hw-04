71453f196a62da1c7e5c7a9c05d52450e6cc28a7
Volodymyr-Stupnyk <vstupnik44@gmail.com>
2025-04-25 18:23:20 +0300

    4

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,86 @@
-import React, { useState, useEffect } from 'react'; 
-import ContactForm from './components/ContactForm/ContactForm';
-import SearchBox from './components/SearchBox/SearchBox';
-import ContactList from './components/ContactList/ContactList';
-import initialContacts from './assets/Contacts.json'
+import { useEffect, useRef, useState } from 'react';
+import { Toaster } from 'react-hot-toast';
+import { getImages } from './components/appi';
+import SearchBar from './components/SearchBar/SearchBar';
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import ImageGallery from './components/ImageGallery/ImageGallery';
+import ImageModal from './components/ImageModal/ImageModal';
+import Loader from './components/Loader/Loader';
+import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';
+import ReactModal from 'react-modal';
 
+ReactModal.setAppElement('#root');
 
+const App = () => {
 
-
-const App = () => { 
-
-  const [contacts, setContacts] = useState(() => {
-    const saved = localStorage.getItem('saved-contact');
-    return saved ? JSON.parse(saved) : initialContacts;
-  });
   const [search, setSearch] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [modalImage, setModalImage] = useState(null);
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isError, setIsError] = useState(false);
+  const pagesCountRef = useRef(0);
+  const prevSearchRef = useRef('');
 
   useEffect(() => {
-    window.localStorage.setItem('saved-contact', JSON.stringify(contacts));
-  }, [contacts]);
+    if (!search) return;
+    if (prevSearchRef.current === search && page === 1) return;
+    addNextImages();
+    prevSearchRef.current = search;
+  }, [search, page]);
 
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(search.toLowerCase())
-  );
-
-  const addContact = (newContact) => {
-    setContacts((prevContacts) => {
-      return [...prevContacts, newContact];
-    });
+  const addNextImages = async () => {
+    try {
+      setIsLoading(true);
+      setIsError(false);
+      const response = await getImages(search, page);
+      pagesCountRef.current = response.total_pages;
+      setImages(prev => [...prev, ...response.results]);
+    } catch (err) {
+      console.error(err);
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
-  const deleteContact = (id) => {
-    setContacts((prevContacts) => {
-      return prevContacts.filter((contact) => contact.id !== id);
-    });
+  const onSubmit = newSearch => {
+    if (newSearch === search) return;
+    setImages([]);
+    setPage(1);
+    setSearch(newSearch);
+    pagesCountRef.current = 0;
   };
 
-  return (  
+  const onLoadMore = () => {
+    setPage(prev => prev + 1);
+  };
+
+  const shouldShowLoadMore =
+    images.length > 0 && page < pagesCountRef.current && !isLoading;
+
+  return (
     <>
-      <h1>Phonebook</h1>
-      <ContactForm addContact={addContact} />
-      <SearchBox  search={search} setSearch={setSearch}/>
-      <ContactList contacts={filteredContacts} onDelete={deleteContact} />
-    </>  
-  );  
+      <Toaster toastOptions={{ error: { iconTheme: { primary: '#FFC107' } } }} position='top-right' />
+      <SearchBar onSubmit={onSubmit} />
+      {isError ? (
+        <ErrorMessage />
+      ) : (
+        <>
+          <ImageGallery
+            images={images}
+            setModalImage={setModalImage}
+          />
+          {shouldShowLoadMore && <LoadMoreBtn onLoadMore={onLoadMore} />}
+        </>
+      )}
+      <Loader isLoading={isLoading} />
+      <ImageModal
+        image={modalImage}
+        onClose={setModalImage}
+      />
+    </>
+  );
 };  
 
 export default App; 
